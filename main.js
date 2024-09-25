a13a7cceafb1b38b6cb7273340bb2d748887a901
Compute64bits <107749070+Compute64bits@users.noreply.github.com>
2024-09-25 22:48:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,17 +14,19 @@ const close_btn = document.getElementById('closeBtn');
 open_btn.addEventListener('click', () => {
     document.getElementsByClassName("only_pc")[0].classList.add('activate')
     const names = document.getElementsByClassName("only_pc")[0].getElementsByTagName("a");
-    names[0].innerHTML = '<i class="fa-solid fa-house"></i> Accueil';
-    names[1].innerHTML = '<i class="fa-solid fa-folder"></i> Projets';
-    names[2].innerHTML = '<i class="fa-solid fa-address-book"></i> Contact';
-    names[3].innerHTML = '<i class="fa-brands fa-github"></i> Github';
+    names[0].innerHTML = '<i class="fa-solid fa-house"></i> À propos';
+    names[1].innerHTML = '<i class="fa-solid fa-eye"></i> Accueil';
+    names[2].innerHTML = '<i class="fa-solid fa-folder"></i> Projets';
+    names[3].innerHTML = '<i class="fa-solid fa-address-book"></i> Contact';
+    names[4].innerHTML = '<i class="fa-brands fa-github"></i> Github';
 });
 
 close_btn.addEventListener('click', () => {
     document.getElementsByClassName("only_pc")[0].classList.remove('activate')
     const names = document.getElementsByClassName("only_pc")[0].getElementsByTagName("a");
     names[0].innerHTML = 'Accueil';
-    names[1].innerHTML = 'Projets';
-    names[2].innerHTML = 'Contact';
-    names[3].innerHTML = 'Github';
+    names[1].innerHTML = 'À propos';
+    names[2].innerHTML = 'Projets';
+    names[3].innerHTML = 'Contact';
+    names[4].innerHTML = 'Github';
 });
